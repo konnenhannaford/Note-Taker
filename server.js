bc2bb2d8df5b62f2e34f211ad7fdb85dc5339b23
konnenhannaford <konnen23@gmail.com>
2021-11-09 19:10:34 +1100

    updated PORT

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const path = require('path');
-const fs = require("fs");
-
-const PORT = process.env.port || 3001;
-
+// const fs = require("fs");
+// const htmlRoutes = require("./routes/htmlRoutes");
+// const apiRoutes = require("./routes/apiRoutes");
+const api = require('./routes/index.js');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.use('/api', api);
 
 // GET Route for homepage
 app.get('/', (req, res) =>
