364d419c838db4f0187147cbf267520531847690
konnenhannaford <konnen23@gmail.com>
2021-11-09 22:49:01 +1100

    updted index.js code to save notes

--- routes/notes.js
+++ routes/notes.js
@@ -16,23 +16,33 @@ notes.get('/', (req, res)=>{
     readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-notes.post('/', (req, res)=>{
-    readFromFile("./db/db.json", { encoding: "utf-8" }, (err, data) => {
-        if (err) {
-            // console.log("Error writing file");
-            res.status(500);
-        } else {
-            let dataBase = JSON.parse(data);
-            let newNote = req.body
-            newNote.id = uuid();
-            dataBase.push(newNote);
-            // console.log(req.body);
-            writeToFile('./db/db.json', dataBase);
-            res.json(dataBase);
-        }
-    });
-});
+notes.post('/', (req, res) => {
+    console.log(req.body);
+    const { text, title } = req.body;
+    if (req.body) {
+      const newTip = {
+        text,
+        title,
+        id: uuid(),   
+      };
+      readAndAppend(newNote, "./db/db.json");
+      res.json(`note added successfully `);
+    } else {
+      res.error('Error in adding note');
+    }
+  });
 
+  notes.get("/:id", (req, res) => {
+    const noteId = req.params.id;
+    readFromFile("./db/db.json")
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        const result = json.filter((note) => note.id === noteId);
+        return result.length > 0
+          ? res.json(result)
+          : res.json("No note with that ID");
+      });
+  });
 
 //READ database, find note you want to delete and then delete it 
 notes.delete('/:id', (req, res) =>{
@@ -45,5 +55,6 @@ notes.delete('/:id', (req, res) =>{
         res.json(`This note has been deleted ${noteId}`);
       });
   });
+
 
 module.exports = notes;
